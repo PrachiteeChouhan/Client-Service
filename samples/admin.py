bd89da216e9482b1893815f2208323cf8e1fb4fc
PrachiteeChouhan <67609981+PrachiteeChouhan@users.noreply.github.com>
2020-09-29 14:55:41 -0700

    Add files via upload

---
+++ samples/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from samples.models import Service_Provider,Client
+
+# Register your models here.
+admin.site.register(Service_Provider)
+admin.site.register(Client)
